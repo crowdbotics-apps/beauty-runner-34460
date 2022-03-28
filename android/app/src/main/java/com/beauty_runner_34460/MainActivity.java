518aec9ec04eebad66021f98d0cab4b1048db52a
Crowdbotics <team@crowdbotics.com>
2022-03-28 03:58:15 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/beauty_runner_34460/MainActivity.java
@@ -0,0 +1,15 @@
+package com.beauty_runner_34460;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "beauty_runner_34460";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "beauty_runner_34460",
+  url: "https://beauty_runner_34460.botics.co",
+  api: "https://beauty_runner_34460.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
